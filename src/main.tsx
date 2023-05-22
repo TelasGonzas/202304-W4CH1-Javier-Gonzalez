24143f9b3f19c9254ce85f9d4baf11e87875a064
Javier González <jgestarriol@gmail.com>
2023-05-22 18:54:17 +0200

    Add button

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { App } from "./components/App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.querySelector(".container") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
